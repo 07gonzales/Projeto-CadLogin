18267640d02e1b4df540b3ea4343e6d3aefacaec
Gabriel Gonzales <gabrielgonzales0007@gmail.com>
2025-04-11 21:34:35 -0300

    Criando array para salvar os dados, pegando o valor inserido no input e inserindo no array, verificando se existe valor e dando um alert caso contrário

--- main.js
+++ main.js
@@ -3,12 +3,32 @@ function acessar(){
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
     
-
     //verifica se os campos foram preenchidos
 
     if(!loginEmail || !loginSenha){
         alert("Favor, preencher todos os campos de acesso.");
-    }else{
+    } else{
+        window.location.href='cadastro.html';
+    }
+}
+
+// Declara array para o projeto
+
+var dadosLista = [];
+
+//função para armazenar nomes em array
+
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+    
+
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+        // console.log(dadosLista);
+
+        document.getElementById('nomeUser').value = "";
         
+    }else {
+        alert("Favor, preencher o nome.");
     }
 }
